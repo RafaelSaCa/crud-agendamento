ecb222cf67d03f28cd813bab9ce8c624907b5382
Rafael Saca <raffaellsaca@gmail.com>
2024-07-17 22:48:25 -0300

    feat: Módulo de pacientes + componente e rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'pacientes' },
+  {
+    path: 'pacientes',
+    loadChildren: () => import('./pacientes/pacientes.module').then(m => m.PacientesModule)
+  },
+];
