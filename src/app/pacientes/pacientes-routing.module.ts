607f9c9fa2f4de0b4e2c786c65076179170bf7c9
Rafael Saca <raffaellsaca@gmail.com>
2024-07-21 21:23:32 -0300

    .

--- src/app/pacientes/pacientes-routing.module.ts
+++ src/app/pacientes/pacientes-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PacientesComponent } from './pacientes/pacientes.component';
 
 const routes: Routes = [
-  {  path:'', component: PacientesComponent }
+  {  path:'', component: PacientesComponent },
 ];
 
 @NgModule({
