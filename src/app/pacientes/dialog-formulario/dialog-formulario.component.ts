c57149cd39bb73d2422a5beba34399ec811de248
Rafael Saca <raffaellsaca@gmail.com>
2024-07-21 21:21:38 -0300

    feat: Salvando dados na api -  mensagem de sucesso e erro

--- src/app/pacientes/dialog-formulario/dialog-formulario.component.ts
+++ src/app/pacientes/dialog-formulario/dialog-formulario.component.ts
@@ -7,6 +7,9 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { PacienteService } from '../services/paciente.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-dialog-formulario',
   standalone: true,
@@ -18,21 +21,48 @@ import { MatInputModule } from '@angular/material/input';
     MatDividerModule,
     MatButtonModule,
     MatIconModule,
+    MatSnackBarModule,
   ],
   templateUrl: './dialog-formulario.component.html',
   styleUrl: './dialog-formulario.component.scss',
 })
 export class DialogFormularioComponent {
   form: FormGroup;
+
   constructor(
     private formBuilder: FormBuilder,
-    public dialogRef: MatDialogRef<DialogFormularioComponent> ) {
+    private service: PacienteService,
+    private snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<DialogFormularioComponent>
+  ) {
     this.form = this.formBuilder.group({
       nome: [''],
       sobrenome: [''],
       cpf: [''],
       telefone: [''],
       email: [''],
+    });
+ }
+  onSubmit() {
+    this.service.save(this.form.value).subscribe(
+      (dados) => this.snackBarSucesso(),
+      (error) => this.snackBarErro()
+    );
+    this.dialogRef.close();
+  }
+
+  private snackBarSucesso() {
+    this.snackBar.open('Paciente cadastrado com sucesso!', '', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
+  private snackBarErro() {
+    this.snackBar.open('Ocorreu um erro ao cadastrar o paciente!', '', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
     });
   }
 }
