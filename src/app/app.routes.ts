4e8d3ef78681e9088c19c3b6a9e9b8fd698f1d3e
Rafael Saca <raffaellsaca@gmail.com>
2024-07-31 22:13:26 -0300

    feat: Rota de agendamentos e procedimentos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,21 @@ export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'pacientes' },
   {
     path: 'pacientes',
-    loadChildren: () => import('./pacientes/pacientes.module').then(m => m.PacientesModule)
+    loadChildren: () =>
+      import('./pacientes/pacientes.module').then((m) => m.PacientesModule),
   },
   {
-    path:'agendamentos',
-    loadChildren: () => import('./agendamentos/agendamentos.module').then(m => m.AgendamentosModule)
-  }
+    path: 'agendamentos',
+    loadChildren: () =>
+      import('./agendamentos/agendamentos.module').then(
+        (m) => m.AgendamentosModule
+      ),
+  },
+  {
+    path: 'procedimentos',
+    loadChildren: () =>
+      import('./procedimentos/procedimentos.module').then(
+        (m) => m.ProcedimentosModule
+      ),
+  },
 ];
